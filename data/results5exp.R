10526befe5da75f9d8d6f803fbd5f2f9ef866bf1
xaviliz <xavilizarraga@gmail.com>
2015-04-12 21:27:30 +0200

    Statistics from experiments

---
+++ data/results5exp.R
@@ -0,0 +1,64 @@
+         Experiment1
+nclicks           21
+UserA             10
+AgentA             0
+ShadowA            0
+ObjA               7
+UserB             11
+AgentB             5
+ShadowB            1
+ObjB               3
+Reciproc           0
+
+
+
+         Experiment2
+nclicks           30
+UserA             10
+AgentA             7
+ShadowA            3
+ObjA               4
+UserB             20
+AgentB            11
+ShadowB            5
+ObjB               9
+Reciproc           6
+> 
+
+
+
+         Experiment3
+nclicks           16
+UserA              7
+AgentA             6
+ShadowA            0
+ObjA               1
+UserB              9
+AgentB             1
+ShadowB            2
+ObjB               1
+Reciproc           5
+
+         Experiment4
+nclicks           19
+UserA             10
+AgentA             5
+ShadowA            3
+ObjA               2
+UserB              9
+AgentB             3
+ShadowB            0
+ObjB               0
+Reciproc           3
+
+         Experiment5
+nclicks           55
+UserA             33
+AgentA            12
+ShadowA            2
+ObjA              18
+UserB             22
+AgentB             8
+ShadowB            5
+ObjB               6
+Reciproc           8
